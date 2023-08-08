99d5eaa2ecf57e7bc16d01a6c067e728569eedbe
Hossein Pira <120723120+ReactMVC@users.noreply.github.com>
2023-08-08 22:07:24 +0330

    2.0.0

--- index.php
+++ index.php
@@ -6,21 +6,23 @@ Email: h3dev.pira@gmail.com
 Github: https://github.com/ReactMVC
 */
 
-// Set the Telegram bot token and name
-$token = 'YOUR_BOT_TOKEN';
+// Set the Telegram bot token and name and channel
+$token = 'token';
 $botname = 'Google Bard';
+$channel_username = '@StealthySolutions';
 
 // Set the API URL for Telegram
 $api_url = 'https://api.telegram.org/bot' . $token . '/';
 
 // Function to send a message to a chat ID
-function sendMessage($chat_id, $text)
+function sendMessage($chat_id, $text, $reply_to_message_id = null)
 {
     global $api_url;
     $url = $api_url . 'sendMessage';
     $post_fields = array(
         'chat_id' => $chat_id,
-        'text' => $text
+        'text' => $text,
+        'reply_to_message_id' => $reply_to_message_id
     );
 
     // Initialize a cURL session
@@ -57,20 +59,86 @@ function sendMessage($chat_id, $text)
 // Function to process an incoming message
 function processMessage($message)
 {
-    global $botname;
+    global $botname, $token, $channel_username;
 
     // Check if the message contains text
     if (isset($message['text'])) {
-        $chat_id = $message['chat']['id'];
         $message_text = $message['text'];
+        if ($message['chat']['type'] == 'private') {
+            $chat_id = $message['chat']['id'];
 
-        // If the message is the "/start" command, greet the user and provide instructions
-        if ($message_text == '/start') {
-            $welcome_message = "به ربات $botname خوش آمدید!\n\nسوال خود را از هوش مصنوعی بارد بپرسید.";
+            // Check if the user is a member of the channel
+            $user_id = $message['from']['id'];
+            $response = json_decode(file_get_contents("https://api.telegram.org/bot$token/getChatMember?chat_id=$channel_username&user_id=$user_id"), true);
+            if ($response['ok'] != true || !in_array($response['result']['status'], ['member', 'administrator', 'creator'])) {
+                // If the user is not a member, admin or creator, send a message asking them to join the channel
+                $join_message = "برای استفاده از ربات، لطفا به کانال $channel_username بپیوندید.\n\nدر گروه خود نیاز به عضویت در کانال ما نیست. اما اگر می خواهید در ربات چت کنید نیاز است عضو کانال ما شوید.";
+                sendMessage($chat_id, $join_message, $message['message_id']);
+                return;
+            }
 
-            sendMessage($chat_id, $welcome_message);
+            // If the message is the "/start" command, greet the user and provide instructions
+            if ($message_text == '/start') {
+                $welcome_message = "به ربات $botname خوش آمدید!\n\nسوال خود را از هوش مصنوعی قدرتمند بپرسید.\nاگر دوست داری منو ببری گروهت به شکل زیر از من در گروه استفاده کن:\nbard: YOUR_TEXT";
+
+                sendMessage($chat_id, $welcome_message, $message['message_id']);
+            } else {
+                // If the message is not the "/start" command, send it to the BardAI
+                $api_url = 'https://api.safone.me/bard?message=' . urlencode($message_text);
+
+                // Initialize a cURL session
+                $ch = curl_init();
+
+                // Set the cURL options
+                // Set the URL to send the GET request to
+                curl_setopt($ch, CURLOPT_URL, $api_url);
+                // Return the response instead of printing it
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+                // Send a message to the user to indicate that their message is being processed
+                sendMessage($chat_id, 'کمی صبر کنید...', $message['message_id']);
+
+                // Send the GET request with cURL and get the response
+                $result = curl_exec($ch);
+
+                // Check if there was an error with the cURL request
+                if (!$result) {
+                    sendMessage($chat_id, 'خطا در برقراری ارتباط.', $message['message_id']);
+                    return;
+                }
+
+                // Decode the JSON response into an associative array
+                $response = json_decode($result, true);
+
+                // Check if the response contains an answer field
+                if (!isset($response['message'])) {
+                    sendMessage($chat_id, 'خطا در دریافت پاسخ.', $message['message_id']);
+                    return;
+                }
+
+                // If the answer is longer than 4096 characters, split it into multiple messages
+                $message_length = strlen($response['message']);
+                if ($message_length > 4096) {
+                    $split_message = str_split($response['message'], 4096);
+                    foreach ($split_message as $msg) {
+                        sendMessage($chat_id, $msg, $message['message_id']);
+                    }
+                } else {
+                    // If the message is shorter than 4096 characters, send it in a single message
+                    sendMessage($chat_id, $response['message'], $message['message_id']);
+                }
+            }
         } else {
-            // If the message is not the "/start" command, send it to the Google Bard AI
+            // Check if the message starts with "bard:"
+            if (!preg_match('/^bard:/i', $message_text)) {
+                return;
+            }
+            // Remove "bard:" from the message text
+            $message_text = preg_replace('/^bard:\s*/i', '', $message_text);
+
+            $chat_id = $message['chat']['id'];
+
+            // send it to the BardAI
             $api_url = 'https://api.safone.me/bard?message=' . urlencode($message_text);
 
             // Initialize a cURL session
@@ -83,36 +151,36 @@ function processMessage($message)
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
             // Send a message to the user to indicate that their message is being processed
-            sendMessage($chat_id, 'کمی صبر کنید...');
+            sendMessage($chat_id, 'کمی صبر کنید...', $message['message_id']);
 
             // Send the GET request with cURL and get the response
             $result = curl_exec($ch);
 
             // Check if there was an error with the cURL request
             if (!$result) {
-                sendMessage($chat_id, 'خطا در برقراری ارتباط.');
+                sendMessage($chat_id, 'خطا در برقراری ارتباط.', $message['message_id']);
                 return;
             }
 
             // Decode the JSON response into an associative array
             $response = json_decode($result, true);
 
-            // Check if the response contains a message field
+            // Check if the response contains an answer field
             if (!isset($response['message'])) {
-                sendMessage($chat_id, 'خطا در دریافت پاسخ.');
+                sendMessage($chat_id, 'خطا در دریافت پاسخ.', $message['message_id']);
                 return;
             }
 
-            // If the message is longer than 4096 characters, split it into multiple messages
+            // If the answer is longer than 4096 characters, split it into multiple messages
             $message_length = strlen($response['message']);
             if ($message_length > 4096) {
                 $split_message = str_split($response['message'], 4096);
                 foreach ($split_message as $msg) {
-                    sendMessage($chat_id, $msg);
+                    sendMessage($chat_id, $msg, $message['message_id']);
                 }
             } else {
                 // If the message is shorter than 4096 characters, send it in a single message
-                sendMessage($chat_id, $response['message']);
+                sendMessage($chat_id, $response['message'], $message['message_id']);
             }
         }
     }
